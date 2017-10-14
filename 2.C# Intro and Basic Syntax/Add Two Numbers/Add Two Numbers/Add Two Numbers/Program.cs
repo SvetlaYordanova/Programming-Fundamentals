600c0f3d2513137c7e920c00ab12047fbafd77bb
SvetlaYordanova <jordanova-svetla>
2017-10-14 17:18:18 +0300

    add
    add

---
+++ 2.C# Intro and Basic Syntax/Add Two Numbers/Add Two Numbers/Add Two Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Add_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = a + b;
+
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+        }
+    }
+}
