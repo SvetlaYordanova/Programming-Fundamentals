d26c1412c65694c9727931db9b62002b81cb0d22
SvetlaYordanova <32373038+SvetlaYordanova@users.noreply.github.com>
2018-05-28 12:39:36 +0300

    Update Program.cs

--- 9.Data Types and Variables - Exercises/Centuries to Nanoseconds/Centuries to Nanoseconds/Centuries to Nanoseconds/Program.cs
+++ 9.Data Types and Variables - Exercises/Centuries to Nanoseconds/Centuries to Nanoseconds/Centuries to Nanoseconds/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Centuries_to_Nanoseconds
 {
-    class Program
+    class Centuries_to_Nanoseconds
     {
         static void Main(string[] args)
         {
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422); // tropical year
+            int hours = days * 24;
+            int minutes = hours * 60;
+            ulong seconds = (ulong)minutes * 60;
+            ulong milliseconds = seconds * 1000;
+            decimal microseconds = milliseconds * 1000;
+            decimal nanoseconds = microseconds * 1000;
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
         }
     }
 }
